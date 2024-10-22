18cd41fba18d84e4e85154948cfb318b92547804
Rob Pickerill <r.pickerill@gmail.com>
2024-10-22 10:36:46 +0100

    first pass knn

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    python311
+    python311Packages.pip
+    python311Packages.virtualenv
+  ];
+
+  shellHook = ''
+    python --version
+  '';
+}
